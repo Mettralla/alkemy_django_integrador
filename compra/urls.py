25d4fe8ea03bcf265a9de6949a8e07d2d8e07622
Mettralla <67493167+Mettralla@users.noreply.github.com>
2023-05-15 17:27:06 -0300

    create producto and proveedor page style added

--- compra/urls.py
+++ compra/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.index, name='productos'),
+    path('productos/listado', views.index, name='productos'),
     path('proveedor/nuevo/', views.new_proveedor, name='nuevo_proveedor'),
     path('producto/nuevo/', views.new_producto, name='nuevo_producto')
     
