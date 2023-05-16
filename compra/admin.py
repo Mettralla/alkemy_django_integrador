727fdf3725ce82df57c84f80ef37f212f9fe8871
Mettralla <67493167+Mettralla@users.noreply.github.com>
2023-05-15 21:41:17 -0300

    mdoels added to admin panel

--- compra/admin.py
+++ compra/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Producto, Proveedor
 
 # Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "precio", "stock_actual", "proveedor")
+
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "apellido", "dni")
+
+admin.site.register(Proveedor, ProveedorAdmin)
+admin.site.register(Producto, ProductoAdmin)
--- compra/urls.py
+++ compra/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='productos'),
     path('productos/listado', views.index, name='productos'),
     path('proveedor/nuevo/', views.new_proveedor, name='nuevo_proveedor'),
     path('producto/nuevo/', views.new_producto, name='nuevo_producto'),
     path('proveedores/listado', views.proveedor_list, name='proveedores')
-    
 ]
